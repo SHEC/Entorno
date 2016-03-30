34973249a4cea615edc423531a8dcc2d9cf3330d
ivan-rangel <ivan05rangel@gmail.com>
2016-03-30 13:32:54 -0600

    Curves version

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'js')));
 
 app.use('/', routes);
 app.use('/users', users);
