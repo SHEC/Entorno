9db5c374eed280595b1dbb4a3f048271fcfe123a
ivan-rangel <ivan05rangel@gmail.com>
2016-04-28 15:12:34 -0500

    new routes defined

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'SHEC' });
 });
 
+/*asaaaaaaaaaaaaaaa*/
+router.get('/:user_name', function(req, res, next) {
+  models.User.findOne({
+    where:{username: req.params.user_name}
+  }).then(function(user){
+    if(user){
+      res.render('user-shec',{
+        user: user
+      });
+    }
+    else
+      res.send("Bad!!!!");
+  });
+});
+
+/*Log in*/
+router.get('/login', function(req, res, next) {
+  models.User.findOne({
+    where:{
+      username: req.body.username, 
+      $and:[{password: req.body.password}] 
+    }
+  }).then(function(user){
+    res.send(user);
+  });
+});
+
+
 
 module.exports = router;
