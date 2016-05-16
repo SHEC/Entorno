fdd63d1d03f259d5b389e27d07bd21d24ef1ef77
ivan-rangel <ivan05rangel@gmail.com>
2016-05-16 00:32:27 -0500

    endpoint changed to allow create session with time parameter

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,14 @@ router.get('/', function(req, res, next) {
 
 
 /*Get to an specific User!!*/
-router.get('/:user_name', function(req, res, next) {
+router.get('/:user_name', function(req, res, next){
   models.User.findOne({
     where:{username: req.params.user_name}
-  }).then(function(user){
+  }).then(function (user){
     if(user){
       console.log('usuario encontrado: '+ user.username);
-      res.render("user-shec",{
-        user: user
-        })
-    }
-    else{
+      res.render("user-shec",{user: user});
+    }else{
       console.log("usuario no encontrado")
       res.redirect("/")
     }
@@ -29,29 +26,27 @@ router.get('/:user_name', function(req, res, next) {
 
 
 /*Create Performance route*/
-router.get('/:user_name/create_performance', function(req, res, next) {
-  models.User.findOne({
-    where: {username: req.params.user_name},
-  }).then(function(user){
-    if(user){
-      models.Session.create({
-        datesession: new Date(),
-        UserId: user.id
-      }).then(function(){
-        models.User.findOne({
-          where: {username: req.params.user_name},
-          include:[models.Session]
-        }).then(function(){
-          console.log("Sesión creada")
-          res.redirect("/"+req.params.user_name+"/performance")
-        });
+router.get('/:user_name/create_performance/:time', function(req, res, next) {
+    if (req.xhr){
+      models.User.findOne({
+        where: {username: req.params.user_name},
+      }).then(function(user){
+        if(user){
+          models.Session.create({
+            datesession: new Date(),
+            timesession: req.params.time,
+            UserId: user.id
+          }).then(function(){
+              res.redirect("/"+req.params.user_name+"/performance")
+            });
+        }else{
+          console.log("Error al crear la sesión creada")
+          res.redirect("/")
+        }
       });
+    }else{
+      console.log("no xhr")
     }
-    else{
-      console.log("Error al crear la sesión creada")
-      res.redirect("/")
-    }
-  });
 });
 
 /*Get Performance route*/
@@ -62,7 +57,7 @@ router.get('/:user_name/performance', function(req, res, next) {
   })
   .then(function(user){
     if(user){
-      console.log('usuario encontrado: '+ user.username+ " con la sesionID:"+ user.Sessions.id);
+      console.log('usuario encontrado: '+ user.username+ " con la sesionID:"+ user.Sessions.datesession);
       res.render("performance", {user:user})
     }
     else{
